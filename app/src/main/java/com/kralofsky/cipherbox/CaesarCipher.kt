278031f32f841c317aa38d8cb67ebe7d72e24cba
Bernhard Kralofsky <bernhard@kralofsky.com>
2019-09-24 09:20:18 +0300

    implement simple caesar cipher

--- app/src/main/java/com/kralofsky/cipherbox/CaesarCipher.kt
+++ app/src/main/java/com/kralofsky/cipherbox/CaesarCipher.kt
@@ -2,46 +2,14 @@ package com.kralofsky.cipherbox
 
 object CaesarCipher : Cipher() {
     override fun encode(cleartext: String): String {
-        return cleartext.mapCharByChar { it + 'a' }
+        return cleartext.mapCharByChar { it + 'b' }
     }
 
     override fun decode(ciphertext: String): String {
-        return ciphertext.mapCharByChar { it - 'a' }
+        return ciphertext.mapCharByChar { it + 'b'.inverse() }
     }
 
     override val name = "Caesar Cipher"
     override val description = ""
     override val imageId = R.mipmap.ic_launcher_round
-}
-
-fun String.mapCharByChar(transform: (Char) -> Char) : String {
-    return this.map(transform).joinToString("") { it.toString() }
-}
-
-operator fun Char.plus(other: Char) : Char {
-    return this.transformCaseIndependent {
-        (it.toAlphabetInt() + other.toAlphabetInt()).toAlphabetChar()
-    }
-}
-
-operator fun Char.minus(other: Char) : Char {
-    return this.transformCaseIndependent {
-        (it.toAlphabetInt() - other.toAlphabetInt()).toAlphabetChar()
-    }
-}
-
-fun Char.toAlphabetInt(): Int {
-    if (!this.isLetter()) return 0
-    return this.toUpperCase().toInt() - 64
-}
-
-fun Int.toAlphabetChar(): Char {
-    return (this + 64).toChar()
-}
-
-fun Char.transformCaseIndependent(transform: (Char) -> Char) : Char {
-    if (!this.isLetter()) return this
-    val upperCase = this.isUpperCase()
-    val v = transform.invoke(this.toUpperCase())
-    return if (upperCase) v.toUpperCase() else v.toLowerCase()
 }
---
+++ app/src/main/java/com/kralofsky/cipherbox/CipherUtils.kt
@@ -0,0 +1,34 @@
+package com.kralofsky.cipherbox
+
+
+fun String.mapCharByChar(transform: (Char) -> Char) : String {
+    return this.map(transform).joinToString("") { it.toString() }
+}
+
+operator fun Char.plus(other: Char) : Char {
+    return this.transformCaseIndependent {
+        ((it.toAlphabetInt() + other.toAlphabetInt())%26).toAlphabetChar()
+    }
+}
+
+fun Char.inverse(): Char {
+    return this.transformCaseIndependent {
+        ((26-it.toAlphabetInt()) % 26).toAlphabetChar()
+    }
+}
+
+fun Char.toAlphabetInt(): Int {
+    if (!this.isLetter()) return 0
+    return this.toUpperCase().toInt() - 65
+}
+
+fun Int.toAlphabetChar(): Char {
+    return (this + 65).toChar()
+}
+
+fun Char.transformCaseIndependent(transform: (Char) -> Char) : Char {
+    if (!this.isLetter()) return this
+    val upperCase = this.isUpperCase()
+    val v = transform.invoke(this.toUpperCase())
+    return if (upperCase) v.toUpperCase() else v.toLowerCase()
+}
