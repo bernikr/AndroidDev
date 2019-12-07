d9a6587ac8c486338739c8e6a6d55390ce5ec08b
Bernhard Kralofsky <bernhard@kralofsky.com>
2019-12-07 13:38:31 +0200

    refactor the slider

--- app/src/main/java/com/kralofsky/cipherbox/ciphers/CaesarCipher.kt
+++ app/src/main/java/com/kralofsky/cipherbox/ciphers/CaesarCipher.kt
@@ -8,7 +8,7 @@ import com.kralofsky.cipherbox.*
 object CaesarCipher : Cipher() {
     override val name = "Caesar Cipher"
     override val imageId = R.drawable.ceasar_portrait
-    override val controlLayout = R.layout.cipher_caesar
+    override val controlLayout = R.layout.cipher_slider
     override val youtube = "o6TPx1Co_wg"
     override val link = "https://en.wikipedia.org/wiki/Caesar_cipher"
 
@@ -19,10 +19,13 @@ object CaesarCipher : Cipher() {
     override fun decode(ciphertext: String): String = ciphertext.mapCharByChar { it + currentLetter.inverse() }
 
     override fun init(context: AppCompatActivity) {
-        context.findViewById<SeekBar>(R.id.cipher_caesar_seekbar).setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        val slider = context.findViewById<SeekBar>(R.id.cipher_slider_seekbar)
+        slider.max = 'Z'.toAlphabetInt()
+
+        slider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 currentLetter = p1.toAlphabetChar()
-                context.findViewById<TextView>(R.id.cipher_caesar_currentLetter).text = currentLetter.toString()
+                context.findViewById<TextView>(R.id.cipher_slider_currentValue).text = "$currentLetter / $p1"
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
